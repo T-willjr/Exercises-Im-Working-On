31ff17cdcee900a1730dda7f1f590ece30d2cde9
Timothy Williams <williamsjr.timothy1@gmail.com>
2021-11-29 22:40:54 -0500

    'Add Madlib Exercise'

---
+++ lib/mad_lib.rb
@@ -0,0 +1,23 @@
+class Madlib
+
+  def noun(n)
+    n
+  end
+
+  def verb(v)
+    v
+  end
+
+  def adjective(adj)
+    adj
+  end
+
+  def adverb(adv)
+    adv
+  end
+
+  def result
+    "Do you #{verb} your #{adjective} #{noun} #{adverb}? That's hilarious!"
+  end
+
+end
---
+++ spec/mad_lib_spec.rb
@@ -0,0 +1,30 @@
+require "../lib/mad_lib"
+
+RSpec.describe Madlib do
+
+  it 'can create a new mad lib' do
+    madlib = Madlib.new
+
+    madlib.noun("dog")
+    madlib.verb("walk")
+    madlib.adjective("blue")
+    madlib.adverb("quickly")
+    result = madlib.result
+    expected = "Do you walk your blue dog quickly? That's hilarious!"
+
+    expect(result).to eq(expected)
+  end
+
+xit 'can create another mad lib' do
+    madlib = Madlib.new
+
+    madlib.noun("capybara")
+    madlib.verb("pet")
+    madlib.adjective("purple")
+    madlib.adverb("intensely")
+    result = madlib.result
+    expected = "Do you pet your purple capybara intensely? That's hilarious!"
+
+    expect(result).to eq(expected)
+  end
+end
